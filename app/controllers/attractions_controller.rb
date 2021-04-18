649573e900af27e3f80787238fc9a3400d611e7a
Luis Martinez <luis_mmartinez@live.com>
2021-04-18 17:58:29 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -19,27 +19,6 @@ class AttractionsController < ApplicationController
     @user = current_user
     @ride = Ride.new(attraction_id: @attraction.id, user_id: @user.id)
   end
-
-#   def ride
-#     @attraction = Attraction.find(params[:id])
-#     @user = current_user
-#     if @user.tall_enough(@attraction) && @user.enough_tickets(@attraction)
-#       @user.tickets -= @attraction.tickets
-#       @user.save
-#       render :'users/show'
-#     elsif !@user.tall_enough(@attraction) && !@user.enough_tickets(@attraction)
-#       @user.errors[:height] << "You are not tall enough to ride the #{@attraction.name}"
-#       @user.errors[:tickets] << "You do not have enough tickets to ride the #{@attraction.name}"
-#       render :'users/show'
-#     elsif !@user.enough_tickets(@attraction)
-#       @user.errors[:tickets] << "You do not have enough tickets to ride the #{@attraction.name}"
-#       render :'users/show'
-#     elsif !@user.tall_enough(@attraction)
-#         #render :'attractions/show'
-#       @user.errors[:height] << "You are not tall enough to ride the #{@attraction.name}"
-#       render :'users/show'
-#     end
-#   end
  
   def edit
     @attraction = Attraction.find(params[:id])
